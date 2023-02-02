1d8a1b077aad398da8ac97490c74169bec9ffc01
Administrator <Administrator@wolfsenpai>
2023-02-02 20:30:32 +0530

    first commit

---
+++ src/com/inheritance/Parent.java
@@ -0,0 +1,20 @@
+package com.inheritance;
+
+public class Parent {
+	
+	public void bike() {
+		
+		System.out.println("vespa");
+		
+	}
+	
+	public void land() {
+		System.out.println("5 acre");
+		
+	}
+	
+	
+	
+	
+
+}
